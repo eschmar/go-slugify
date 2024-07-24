36900fe689b596096bb4ccf474eb35a052744d1b
Marcel Eschmann <marcel.eschmann@gmail.com>
2024-07-24 13:25:53 +0200

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/eschmar/go-slugify
+
+go 1.22.2
---
+++ slugify.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"path"
+	"strings"
+
+	"github.com/eschmar/go-slugify/slug"
+)
+
+// ANSI coloured output modes
+const (
+	ansiReset          = "\033[0m"
+	ansiFaint          = "\033[2m"
+	ansiResetFaint     = "\033[22m"
+	ansiBrightRed      = "\033[91m"
+	ansiBrightGreen    = "\033[92m"
+	ansiBrightYellow   = "\033[93m"
+	ansiBrightRedFaint = "\033[91;2m"
+)
+
+func main() {
+	inputs := os.Args[1:]
+
+	total := len(inputs)
+	renamed := 0
+
+	// Iterate over each input and treat as file path
+	for _, input := range inputs {
+		// Check if valid file or directory
+		info, err := os.Stat(input)
+		if err != nil {
+			reason := errors.Unwrap(err)
+			fmt.Printf(" %sx%s %s %s(%v)%s\n", ansiBrightRed, ansiReset, input, ansiBrightRedFaint, reason, ansiReset)
+			continue
+		}
+
+		// Extract base name and extension, then slugify
+		extension := path.Ext(input)
+		name := strings.TrimSuffix(path.Base(input), extension)
+
+		result := slug.Ify(name)
+		if !info.IsDir() {
+			result = result + extension
+		}
+
+		// Rename file or directory
+		err = os.Rename(input, result)
+
+		if err == nil {
+			fmt.Printf(" %s✓%s %s %s(%s)%s\n", ansiBrightGreen, ansiReset, result, ansiFaint, input, ansiReset)
+			renamed += 1
+			continue
+		}
+
+		fmt.Printf(" %sx%s %s %s(%s)%s\n", ansiBrightRed, ansiReset, input, ansiBrightRedFaint, err.Error(), ansiReset)
+	}
+
+	fmt.Printf("%sRenamed %d/%d.%s\n", ansiBrightYellow, renamed, total, ansiReset)
+}
