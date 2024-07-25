184c342a11d6b1e3d599f679d10b628516458922
Marcel Eschmann <marcel.eschmann@gmail.com>
2024-07-25 15:36:33 +0200

    Add test case

---
+++ slug/slug_test.go
@@ -0,0 +1,39 @@
+package slug
+
+import (
+	"fmt"
+	"testing"
+)
+
+func truncate(in string, max int) string {
+	if len(in) < max {
+		return in
+	}
+
+	return in[:max] + "..."
+}
+
+func TestSlugify(t *testing.T) {
+	tests := []struct {
+		in, verify string
+	}{
+		{
+			" Importànt Dôcument (³)_copy final [36900fe]-compressed (((((8)))))---",
+			"Important-Document-3-copy-final-36900fe-compressed-8",
+		},
+		{
+			"Schwiizerdütsch & svensk omljud ä ö å behandling",
+			"Schwiizerduetsch-and-svensk-omljud-ae-oe-a-behandling",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(fmt.Sprintf("Test '%s'", truncate(test.in, 10)), func(t *testing.T) {
+			out := Ify(test.in)
+
+			if out != test.verify {
+				t.Errorf("got '%s', want '%s'", out, test.verify)
+			}
+		})
+	}
+}
